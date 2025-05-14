a6dd6c1b18f2749d57c0a16947644cf8e51e9c4c
yoad-hordan4 <your-email@example.com>
2025-05-14 14:52:32 +0300

    not working

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from routes import trails
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-app = FastAPI()
-
-# Mount frontend
-experience_dir = Path(__file__).resolve().parent.parent / "experience"
-app.mount("/experience", StaticFiles(directory=experience_dir, html=True), name="experience")
 app = FastAPI(
     title="StrideQuest API",
     description="API for interactive, location-based trail experiences",
     version="0.1.0"
 )
+
+# Mount frontend
+experience_dir = Path(__file__).resolve().parents[1] / "experience"
+app.mount("/experience", StaticFiles(directory=experience_dir, html=True), name="experience")
 
 # Include modular routes
 app.include_router(trails.router, prefix="/trails", tags=["Trails"])
